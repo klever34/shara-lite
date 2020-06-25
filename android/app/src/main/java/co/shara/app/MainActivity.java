6fc14030243b84023471109bd26673b262a46654
Tioluwani Kolawole <kolawole8tiolu@gmail.com>
2020-06-25 17:40:26 +0100

    change application name and package name

--- android/app/src/main/java/co/shara/app/MainActivity.java
+++ android/app/src/main/java/co/shara/app/MainActivity.java
@@ -1,4 +1,4 @@
-package com.shararn;
+package co.shara.app;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "SharaRN";
+    return "Shara";
   }
 }
--- android/app/src/main/java/co/shara/app/MainApplication.java
+++ android/app/src/main/java/co/shara/app/MainApplication.java
@@ -1,4 +1,4 @@
-package com.shararn;
+package co.shara.app;
 
 import android.app.Application;
 import android.content.Context;
@@ -62,7 +62,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.shararn.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("co.shara.app.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
