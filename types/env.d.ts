3eb452399f17efa7e4fe8f354626d2c1f536184e
Wahab Bello <wahaaabello@gmail.com>
2020-06-27 17:09:38 +0100

    chore: fix eslint issues

--- types/env.d.ts
+++ types/env.d.ts
@@ -1,5 +1,5 @@
 declare module 'react-native-dotenv' {
-    export const PUBNUB_SUB_KEY: string
-    export const PUBNUB_PUB_KEY: string
-    export const API_BASE_URL: string
+  export const PUBNUB_SUB_KEY: string;
+  export const PUBNUB_PUB_KEY: string;
+  export const API_BASE_URL: string;
 }
