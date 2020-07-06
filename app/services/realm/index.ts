5272b89003fc106d7a13c18207d7a5427fc6ac06
Wahab Bello <wahaaabello@gmail.com>
2020-07-06 19:00:31 +0100

    feature: refactor useRealm

--- app/services/realm/index.ts
+++ app/services/realm/index.ts
@@ -6,9 +6,9 @@ const RealmContext = createContext<Realm | null>(null);
 export const RealmProvider = RealmContext.Provider;
 export const useRealm = () => {
   const realm = useContext(RealmContext) as Realm;
-  const [, setToggle] = useState(false);
+  const [, rerender] = useState(false);
   useEffect(() => {
-    const listener = () => setToggle((prevToggle) => !prevToggle);
+    const listener = () => rerender((flag) => !flag);
     realm.addListener('change', listener);
     return () => {
       realm.removeListener('change', listener);
