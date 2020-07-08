9baca2b4788c31d4dcdffdb55adfdde6283b2493
Tioluwani Kolawole <kolawole8tiolu@gmail.com>
2020-07-08 09:37:43 +0100

    fix lint issues

--- app/styles/variables.ts
+++ app/styles/variables.ts
@@ -1,9 +1,9 @@
-import {Dimensions} from 'react-native'
+import {Dimensions} from 'react-native';
 
 export const dimensions = {
   fullHeight: Dimensions.get('window').height,
   fullWidth: Dimensions.get('window').width,
-}
+};
 
 export const colors = {
   primary: '#dd0404',
@@ -20,7 +20,7 @@ export const colors = {
   'gray-800': '#2D3748',
   'gray-900': '#1A202C',
   black: '#222222',
-}
+};
 
 export const spacing: {[key: string]: number} = {
   xs: 4,
@@ -28,4 +28,4 @@ export const spacing: {[key: string]: number} = {
   md: 12,
   lg: 16,
   xl: 24,
-}
+};
