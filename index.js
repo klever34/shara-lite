c8e6f4555807e886b8633618608ba7618fd7f413
Wahab Bello <wahaaabello@gmail.com>
2020-07-28 21:29:32 +0100

    chore: remove release

--- index.js
+++ index.js
@@ -7,12 +7,10 @@ import App from './app/index';
 import {name as appName} from './app.json';
 import * as Sentry from '@sentry/react-native';
 import {YellowBox} from 'react-native';
-import {name, version} from './package.json';
 import Config from 'react-native-config';
 
 Sentry.init({
   dsn: Config.SENTRY_DSN,
-  release: `${name}@${version}`,
   environment: Config.ENVIRONMENT,
 });
 YellowBox.ignoreWarnings(['Setting a timer']);
