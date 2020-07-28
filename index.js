db21e50bd7fa6c637ee7e4154ba5bfc5f02d4fec
Wahab Bello <wahaaabello@gmail.com>
2020-07-28 12:59:22 +0100

    chore: add env variables to sentry

--- index.js
+++ index.js
@@ -8,11 +8,12 @@ import {name as appName} from './app.json';
 import * as Sentry from '@sentry/react-native';
 import {YellowBox} from 'react-native';
 import {name, version} from './package.json';
+import Config from 'react-native-config';
 
 Sentry.init({
-  dsn:
-    'https://394fedbf2e0f4057a3a888c7801d0c60@o426074.ingest.sentry.io/5367463',
+  dsn: Config.SENTRY_DSN,
   release: `${name}@${version}`,
+  environment: Config.SENTRY_ENVIRONMENT,
 });
 YellowBox.ignoreWarnings(['Setting a timer']);
 AppRegistry.registerComponent(appName, () => App);
