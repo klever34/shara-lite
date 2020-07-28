dd935aa2762cbd4004c516c4724f62c9bf638fdf
Olawale Malomo <warlay234@gmail.com>
2020-07-28 21:02:08 +0100

    Add finance service

--- app/services/FinanceService.tsx
+++ app/services/FinanceService.tsx
@@ -1,1 +1,45 @@
-export const getSummary = () => {};
+import {getReceipts} from './ReceiptService';
+import {getCredits} from './CreditService';
+import Realm from 'realm';
+
+export interface ISummary {
+  totalSales: number;
+  totalCredit: number;
+  overdueCredit: number;
+}
+
+export const getSummary = ({realm}: {realm: Realm}): ISummary => {
+  const receipts = getReceipts({realm});
+  const credits = getCredits({realm});
+
+  const totalSales = getTotalSales({receipts});
+  const totalCredit = getTotalCredit({credits});
+  const overdueCredit = getOverdueCredit({credits});
+
+  const summary: ISummary = {
+    totalSales,
+    totalCredit,
+    overdueCredit,
+  };
+
+  return summary;
+};
+
+export const getTotalSales = ({receipts}) => {
+  return receipts.sum('total_amount');
+};
+
+export const getOverdueCredit = ({credits}) => {
+  return credits.filtered('fulfilled = false').sum('amount_left');
+};
+
+export const getTotalCredit = ({credits}) => {
+  return credits.sum('total_amount');
+};
+
+export const getTodayFilter = () => {
+  const startDate = new Date();
+  startDate.setHours(0, 0, 0, 0);
+
+  return `created_at >= ${startDate}`;
+};
