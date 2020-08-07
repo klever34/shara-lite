0b95ecd936c97d5682071806cfb4f8e1c37efa82
Tioluwani Kolawole <kolawole8tiolu@gmail.com>
2020-08-07 01:24:22 +0100

    fix linting issues

--- app/services/CreditService.ts
+++ app/services/CreditService.ts
@@ -1,13 +1,13 @@
-import Realm, {UpdateMode} from 'realm'
-import {ICustomer} from '../models'
-import {ICredit, modelName} from '../models/Credit'
-import {IReceipt} from '../models/Receipt'
-import {getBaseModelValues} from '../helpers/models'
-import {Customer} from '../../types/app'
+import Realm, {UpdateMode} from 'realm';
+import {ICustomer} from '../models';
+import {ICredit, modelName} from '../models/Credit';
+import {IReceipt} from '../models/Receipt';
+import {getBaseModelValues} from '../helpers/models';
+import {Customer} from '../../types/app';
 
 export const getCredits = ({realm}: {realm: Realm}): ICredit[] => {
-  return (realm.objects<ICredit>(modelName) as unknown) as ICredit[]
-}
+  return (realm.objects<ICredit>(modelName) as unknown) as ICredit[];
+};
 
 export const saveCredit = ({
   realm,
@@ -16,11 +16,11 @@ export const saveCredit = ({
   dueDate,
   creditAmount,
 }: {
-  realm: Realm
-  dueDate: Date
-  customer?: ICustomer | Customer
-  receipt?: IReceipt
-  creditAmount: number
+  realm: Realm;
+  dueDate: Date;
+  customer?: ICustomer | Customer;
+  receipt?: IReceipt;
+  creditAmount: number;
 }): void => {
   const credit: ICredit = {
     receipt,
@@ -29,35 +29,35 @@ export const saveCredit = ({
     amount_left: creditAmount,
     amount_paid: 0,
     ...getBaseModelValues(),
-  }
+  };
 
   if (customer && customer.name) {
-    credit.customer_name = customer?.name
-    credit.customer_mobile = customer?.mobile
+    credit.customer_name = customer?.name;
+    credit.customer_mobile = customer?.mobile;
   }
 
   if (customer && customer.id) {
-    credit.customer = customer as ICustomer
+    credit.customer = customer as ICustomer;
   }
 
   realm.write(() => {
-    realm.create<ICredit>(modelName, credit, UpdateMode.Modified)
-  })
-}
+    realm.create<ICredit>(modelName, credit, UpdateMode.Modified);
+  });
+};
 
 export const updateCredit = ({
   realm,
   credit,
   updates,
 }: {
-  realm: Realm
-  credit: ICredit
-  updates: object
+  realm: Realm;
+  credit: ICredit;
+  updates: object;
 }) => {
   const updatedCredit = {
     id: credit.id,
     ...updates,
-  }
+  };
 
-  realm.create<ICredit>(modelName, updatedCredit, UpdateMode.Modified)
-}
+  realm.create<ICredit>(modelName, updatedCredit, UpdateMode.Modified);
+};
