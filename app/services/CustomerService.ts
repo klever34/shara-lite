fd887537958b0b71fc4d2cd46ee2a5cc9729e56e
Olawale Malomo <warlay234@gmail.com>
2020-08-08 19:39:22 +0100

    Save customer only if mobile doesn't exist already

--- app/services/CustomerService.ts
+++ app/services/CustomerService.ts
@@ -1,6 +1,7 @@
 import {ICustomer, modelName} from '../models';
 import Realm, {UpdateMode} from 'realm';
 import {getBaseModelValues} from '../helpers/models';
+import {omit} from 'lodash';
 
 export const getCustomers = ({realm}: {realm: Realm}): ICustomer[] => {
   return (realm.objects<ICustomer>(modelName) as unknown) as ICustomer[];
@@ -12,13 +13,36 @@ export const saveCustomer = ({
 }: {
   realm: Realm;
   customer: ICustomer;
-}): void => {
+}): ICustomer => {
   const customerDetails: ICustomer = {
     ...customer,
     ...getBaseModelValues(),
   };
+  const existingCustomer = getCustomerByMobile({
+    realm,
+    mobile: customerDetails.mobile,
+  });
+
+  if (existingCustomer) {
+    return existingCustomer;
+  }
 
   realm.write(() => {
     realm.create<ICustomer>(modelName, customerDetails, UpdateMode.Modified);
   });
+
+  return customerDetails;
+};
+
+export const getCustomerByMobile = ({
+  realm,
+  mobile,
+}: {
+  realm: Realm;
+  mobile: string;
+}): ICustomer | null => {
+  const foundCustomers = realm
+    .objects<ICustomer>(modelName)
+    .filtered(`mobile = "${mobile}" LIMIT(1)`);
+  return foundCustomers.length ? (omit(foundCustomers[0]) as ICustomer) : null;
 };
