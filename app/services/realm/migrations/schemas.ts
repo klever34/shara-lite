bd617d757185af6e42a449d752ef4d3d6a7dce07
Olawale Malomo <warlay234@gmail.com>
2020-08-31 17:46:47 +0100

    Add migration for is_deleted and credits fulfilled field

--- app/services/realm/migrations/schemas.ts
+++ app/services/realm/migrations/schemas.ts
@@ -1,5 +1,9 @@
 import schema1 from './1598342143007-add-optional-is-deleted-fields';
+import schema2 from './1598342143007-restore-fulfilled-in-credits';
 
-const schemas: any[] = [{schemaVersion: 1, ...schema1}];
+const schemas: any[] = [
+  {schemaVersion: 1, ...schema1},
+  {schemaVersion: 2, ...schema2},
+];
 
 export default schemas;
