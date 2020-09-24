b4c6d7870648f4e61e4b4f37995d815580d91d28
Tioluwani Kolawole <kolawole8tiolu@gmail.com>
2020-09-24 17:53:35 +0100

    add push notification for credit due tomorrow

--- app/services/credit-reminder/hook.ts
+++ app/services/credit-reminder/hook.ts
@@ -1,4 +1,5 @@
 import {useNavigation} from '@react-navigation/native';
+import {isTomorrow} from 'date-fns';
 import {useCallback, useEffect} from 'react';
 import BackgroundFetch from 'react-native-background-fetch';
 import {getNotificationService} from '..';
@@ -16,9 +17,14 @@ export const useCreditReminder = () => {
       credit.due_date && credit?.due_date?.getTime() <= new Date().getTime(),
   );
 
-  const pushNotification = useCallback(() => {
+  const creditDueTomorrow = credits.filter(
+    (credit) => credit.due_date && isTomorrow(credit.due_date),
+  );
+
+  const overdueCreditNotification = useCallback(() => {
     if (realm && overdueCredits.length) {
       notificationService.localNotification({
+        id: 0,
         title: 'Overdue Credit',
         smallIcon: 'ic_launcher',
         message: `You have ${overdueCredits.length} overdue credit`,
@@ -26,16 +32,35 @@ export const useCreditReminder = () => {
     }
   }, [notificationService, overdueCredits, realm]);
 
+  const dueTomorrowNotification = useCallback(() => {
+    const creditNumber = creditDueTomorrow.length;
+    if (realm && creditNumber) {
+      const message =
+        creditNumber > 1
+          ? `You have ${creditDueTomorrow.length} credits due tomorrow. Do you want to send reminders?`
+          : `You have ${creditDueTomorrow.length} credit due tomorrow. Do you want to send a reminder?`;
+      notificationService.localNotification({
+        id: 1,
+        message,
+        smallIcon: 'ic_launcher',
+        title: 'Credit due tomorrow',
+      });
+    }
+  }, [realm, creditDueTomorrow.length, notificationService]);
+
   useEffect(() => {
     BackgroundFetch.configure(
       {
         stopOnTerminate: false,
-        minimumFetchInterval: 1440, // fetch interval in minutes
+        // minimumFetchInterval: 1440, // fetch interval in minutes
+        minimumFetchInterval: 30,
       },
       async (taskId) => {
         console.log('Received background-fetch event: ', taskId);
 
-        pushNotification();
+        overdueCreditNotification();
+
+        dueTomorrowNotification();
 
         BackgroundFetch.finish(taskId);
       },
@@ -47,9 +72,15 @@ export const useCreditReminder = () => {
   }, []);
 
   useEffect(() => {
-    return notificationService.addEventListener(() => {
-      navigation.navigate('OverdueCredit');
-      notificationService.cancelAllLocalNotifications();
+    return notificationService.addEventListener((notification: any) => {
+      if (notification.id === '0') {
+        navigation.navigate('OverdueCredit');
+        notificationService.cancelAllLocalNotifications();
+      }
+      if (notification.id === '1') {
+        navigation.navigate('TotalCredit');
+        notificationService.cancelAllLocalNotifications();
+      }
     });
   }, [navigation, notificationService]);
 };
