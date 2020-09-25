6339b4b040221d8d20707efd8a3f34cb1aa76c2c
Tioluwani Kolawole <kolawole8tiolu@gmail.com>
2020-09-25 14:41:27 +0100

    remove select content event from new receipt

--- app/screens/main/business/receipts/NewReceipt.tsx
+++ app/screens/main/business/receipts/NewReceipt.tsx
@@ -82,12 +82,6 @@ export const NewReceipt = () => {
       }
       setSelectedProduct(item);
       setPrice(item?.price);
-      getAnalyticsService()
-        .logEvent('selectContent', {
-          item_id: item?._id?.toString() ?? '',
-          content_type: 'product',
-        })
-        .then(() => {});
     },
     [receipt],
   );
