6e1cc0422e71f0a08049c4baa9a706dd582b254b
Wahab Bello <wahaaabello@gmail.com>
2020-10-30 13:00:17 +0100

    feature: add more options

--- app-v3/screens/main/more/MoreOptionsScreen.tsx
+++ app-v3/screens/main/more/MoreOptionsScreen.tsx
@@ -1,17 +1,19 @@
-import React, {useLayoutEffect} from 'react';
-import {Text, View} from 'react-native';
+import React, {useCallback, useLayoutEffect, useMemo} from 'react';
+import {Text, View, Image} from 'react-native';
 import {useAppNavigation} from 'app-v3/services/navigation';
-import {applyStyles} from 'app-v3/helpers/utils';
 import {
   HeaderBackButton,
   StackHeaderLeftButtonProps,
 } from '@react-navigation/stack';
 import {Icon} from 'app-v3/components/Icon';
 import {colors} from 'app-v3/styles';
-import {HeaderRight} from 'app-v3/components';
+import {getAuthService} from 'app-v3/services';
+import Touchable from '@/components/Touchable';
+import {applyStyles} from 'app-v3/styles';
 
 export const MoreOptionsScreen = () => {
   const navigation = useAppNavigation();
+  const user = getAuthService().getUser();
   useLayoutEffect(() => {
     navigation.setOptions({
       headerStyle: applyStyles('border-b-1', {
@@ -48,17 +50,128 @@ export const MoreOptionsScreen = () => {
         );
       },
       headerTitle: () => null,
-      headerRight: () => (
-        <HeaderRight
-          menuOptions={[
-            {
-              text: 'Help',
-              onSelect: () => {},
-            },
-          ]}
-        />
-      ),
     });
   }, [navigation]);
-  return <View />;
+
+  const onEdit = useCallback(() => {
+    console.log('foo');
+  }, []);
+
+  const moreOptions = useMemo(() => {
+    return [
+      {
+        title: 'Profile Settings',
+        icon: 'user',
+        onPress: () => {},
+      },
+      {
+        title: 'Business Settings',
+        icon: 'sliders',
+        onPress: onEdit,
+      },
+      {
+        title: 'Reports',
+        icon: 'eye',
+        onPress: () => {},
+      },
+      {
+        title: 'Reminder Settings',
+        icon: 'users',
+        onPress: () => {},
+      },
+      {
+        title: 'App Lock',
+        icon: 'users',
+        onPress: () => {},
+      },
+      {
+        title: 'Help & Support',
+        icon: 'users',
+        onPress: () => {},
+      },
+    ];
+  }, [onEdit]);
+
+  return (
+    <View style={applyStyles('')}>
+      {user && (
+        <View style={applyStyles('p-16 flex-row items-center')}>
+          <View style={applyStyles('w-80 h-80')}>
+            <Image
+              style={applyStyles('w-full h-full')}
+              source={{
+                uri: 'https://reactnative.dev/img/tiny_logo.png',
+              }}
+            />
+          </View>
+          <View style={applyStyles('flex-1 px-12')}>
+            <Text
+              style={applyStyles(
+                'text-700 uppercase text-sm leading-16 text-gray-300 mb-4',
+              )}>
+              {user.businesses[0].name}
+            </Text>
+            <Text
+              style={applyStyles('text-400 text-sm leading-16 mb-4', {
+                color: colors['gray-300'],
+              })}>
+              {user.mobile}
+            </Text>
+            <Text
+              style={applyStyles('text-400 text-xs leading-16 uppercase', {
+                color: colors['gray-100'],
+              })}>
+              ID: {user.id}
+            </Text>
+          </View>
+          <HeaderBackButton
+            onPress={onEdit}
+            backImage={() => (
+              <View style={applyStyles('center w-48 h-48')}>
+                <Icon
+                  type="feathericons"
+                  name="edit"
+                  size={24}
+                  color={colors['gray-50']}
+                  onPress={onEdit}
+                />
+              </View>
+            )}
+          />
+        </View>
+      )}
+      {moreOptions.map(({title, icon, onPress}) => {
+        return (
+          <Touchable onPress={onPress} key={title}>
+            <View
+              style={applyStyles(
+                'flex-row items-center py-16 px-16 border-t-1 border-gray-20',
+              )}>
+              <View style={applyStyles('flex-row flex-1')}>
+                <Icon
+                  type="feathericons"
+                  color={colors['red-100']}
+                  name={icon}
+                  size={20}
+                  style={applyStyles('mr-8')}
+                />
+                <Text
+                  style={applyStyles('text-400 text-sm', {
+                    color: colors['gray-300'],
+                  })}>
+                  {title}
+                </Text>
+              </View>
+              <Icon
+                type="feathericons"
+                color={colors['gray-50']}
+                name="chevron-right"
+                size={20}
+              />
+            </View>
+          </Touchable>
+        );
+      })}
+    </View>
+  );
 };
