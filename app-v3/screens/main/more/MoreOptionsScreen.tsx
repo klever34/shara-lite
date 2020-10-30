e813b9b98e2ca7c81ecb7bca8509eb63b2f383f7
Wahab Bello <wahaaabello@gmail.com>
2020-10-30 13:38:06 +0100

    feature: complete more page layout

--- app-v3/screens/main/more/MoreOptionsScreen.tsx
+++ app-v3/screens/main/more/MoreOptionsScreen.tsx
@@ -1,15 +1,19 @@
-import React, {useCallback, useLayoutEffect, useMemo} from 'react';
-import {Text, View, Image} from 'react-native';
+import React, {useCallback, useContext, useLayoutEffect, useMemo} from 'react';
+import {Text, View, Image, ScrollView} from 'react-native';
 import {useAppNavigation} from 'app-v3/services/navigation';
 import {
   HeaderBackButton,
   StackHeaderLeftButtonProps,
 } from '@react-navigation/stack';
 import {Icon} from 'app-v3/components/Icon';
-import {colors} from 'app-v3/styles';
+import {colors, dimensions} from 'app-v3/styles';
 import {getAuthService} from 'app-v3/services';
-import Touchable from '@/components/Touchable';
+import Touchable from 'app-v3/components/Touchable';
 import {applyStyles} from 'app-v3/styles';
+import {RealmContext} from 'app-v3/services/realm/provider';
+import {useErrorHandler} from 'app-v3/services/error-boundary';
+import {getAnalyticsService} from 'app-v3/services';
+import {version} from '../../../../package.json';
 
 export const MoreOptionsScreen = () => {
   const navigation = useAppNavigation();
@@ -53,9 +57,7 @@ export const MoreOptionsScreen = () => {
     });
   }, [navigation]);
 
-  const onEdit = useCallback(() => {
-    console.log('foo');
-  }, []);
+  const onEditBusinessSettings = useCallback(() => {}, []);
 
   const moreOptions = useMemo(() => {
     return [
@@ -67,7 +69,7 @@ export const MoreOptionsScreen = () => {
       {
         title: 'Business Settings',
         icon: 'sliders',
-        onPress: onEdit,
+        onPress: onEditBusinessSettings,
       },
       {
         title: 'Reports',
@@ -90,88 +92,136 @@ export const MoreOptionsScreen = () => {
         onPress: () => {},
       },
     ];
-  }, [onEdit]);
+  }, [onEditBusinessSettings]);
+
+  const {logoutFromRealm} = useContext(RealmContext);
+  const handleError = useErrorHandler();
+
+  const handleLogout = useCallback(async () => {
+    try {
+      const authService = getAuthService();
+      await authService.logOut();
+      getAnalyticsService().logEvent('logout').catch(handleError);
+      navigation.reset({
+        index: 0,
+        routes: [{name: 'Auth'}],
+      });
+      logoutFromRealm && logoutFromRealm();
+    } catch (e) {
+      handleError(e);
+    }
+  }, [handleError, navigation, logoutFromRealm]);
 
   return (
-    <View style={applyStyles('')}>
-      {user && (
-        <View style={applyStyles('p-16 flex-row items-center')}>
-          <View style={applyStyles('w-80 h-80')}>
-            <Image
-              style={applyStyles('w-full h-full')}
-              source={{
-                uri: 'https://reactnative.dev/img/tiny_logo.png',
-              }}
+    <ScrollView>
+      <View style={applyStyles({minHeight: dimensions.fullHeight - 120})}>
+        {user && (
+          <View style={applyStyles('p-16 flex-row items-center')}>
+            <View style={applyStyles('w-80 h-80')}>
+              <Image
+                style={applyStyles('w-full h-full')}
+                source={{
+                  uri: 'https://reactnative.dev/img/tiny_logo.png',
+                }}
+              />
+            </View>
+            <View style={applyStyles('flex-1 px-12')}>
+              <Text
+                style={applyStyles(
+                  'text-700 uppercase text-sm leading-16 text-gray-300 mb-4',
+                )}>
+                {user.businesses[0].name}
+              </Text>
+              <Text
+                style={applyStyles('text-400 text-sm leading-16 mb-4', {
+                  color: colors['gray-300'],
+                })}>
+                {user.mobile}
+              </Text>
+              <Text
+                style={applyStyles('text-400 text-xs leading-16 uppercase', {
+                  color: colors['gray-100'],
+                })}>
+                ID: {user.id}
+              </Text>
+            </View>
+            <HeaderBackButton
+              onPress={onEditBusinessSettings}
+              backImage={() => (
+                <View style={applyStyles('center w-48 h-48')}>
+                  <Icon
+                    type="feathericons"
+                    name="edit"
+                    size={24}
+                    color={colors['gray-50']}
+                    onPress={onEditBusinessSettings}
+                  />
+                </View>
+              )}
             />
           </View>
-          <View style={applyStyles('flex-1 px-12')}>
-            <Text
-              style={applyStyles(
-                'text-700 uppercase text-sm leading-16 text-gray-300 mb-4',
-              )}>
-              {user.businesses[0].name}
-            </Text>
-            <Text
-              style={applyStyles('text-400 text-sm leading-16 mb-4', {
-                color: colors['gray-300'],
-              })}>
-              {user.mobile}
-            </Text>
-            <Text
-              style={applyStyles('text-400 text-xs leading-16 uppercase', {
-                color: colors['gray-100'],
-              })}>
-              ID: {user.id}
-            </Text>
-          </View>
-          <HeaderBackButton
-            onPress={onEdit}
-            backImage={() => (
-              <View style={applyStyles('center w-48 h-48')}>
-                <Icon
-                  type="feathericons"
-                  name="edit"
-                  size={24}
-                  color={colors['gray-50']}
-                  onPress={onEdit}
-                />
-              </View>
-            )}
-          />
+        )}
+        <View style={applyStyles('mb-32')}>
+          {moreOptions.map(({title, icon, onPress}, index) => {
+            return (
+              <Touchable onPress={onPress} key={title}>
+                <View
+                  style={applyStyles(
+                    'flex-row items-center py-16 px-16 border-t-1 border-gray-20',
+                    index === moreOptions.length - 1 && 'border-b-1',
+                  )}>
+                  <View style={applyStyles('flex-row flex-1')}>
+                    <Icon
+                      type="feathericons"
+                      color={colors['red-100']}
+                      name={icon}
+                      size={20}
+                      style={applyStyles('mr-8')}
+                    />
+                    <Text style={applyStyles('text-400 text-sm text-gray-300')}>
+                      {title}
+                    </Text>
+                  </View>
+                  <Icon
+                    type="feathericons"
+                    color={colors['gray-50']}
+                    name="chevron-right"
+                    size={20}
+                  />
+                </View>
+              </Touchable>
+            );
+          })}
         </View>
-      )}
-      {moreOptions.map(({title, icon, onPress}) => {
-        return (
-          <Touchable onPress={onPress} key={title}>
+        <View style={applyStyles('flex-1')}>
+          <Touchable onPress={handleLogout}>
             <View
               style={applyStyles(
-                'flex-row items-center py-16 px-16 border-t-1 border-gray-20',
+                'flex-row border-1 border-gray-20 center p-16 rounded-md',
               )}>
-              <View style={applyStyles('flex-row flex-1')}>
-                <Icon
-                  type="feathericons"
-                  color={colors['red-100']}
-                  name={icon}
-                  size={20}
-                  style={applyStyles('mr-8')}
-                />
-                <Text
-                  style={applyStyles('text-400 text-sm', {
-                    color: colors['gray-300'],
-                  })}>
-                  {title}
-                </Text>
-              </View>
               <Icon
                 type="feathericons"
                 color={colors['gray-50']}
-                name="chevron-right"
+                name="log-out"
                 size={20}
+                style={applyStyles('mr-24')}
               />
+              <Text style={applyStyles('text-gray-100 text-sm font-bold')}>
+                Log Out
+              </Text>
             </View>
           </Touchable>
-        );
-      })}
-    </View>
+        </View>
+        <View style={applyStyles('py-32 center')}>
+          <Text style={applyStyles('text-gray-50 text-xxs font-bold mb-16')}>
+            Version {version}
+          </Text>
+          <Image
+            source={require('../../../assets/images/shara_logo_grey.png')}
+            style={applyStyles('w-72 h-16')}
+          />
+        </View>
+      </View>
+    </ScrollView>
   );
 };
