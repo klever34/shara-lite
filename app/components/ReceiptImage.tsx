be023f941a8837341c32fe3e07a94a29d98f1b0f
racheal58 <orukele_racheal@yahoo.com>
2020-11-17 23:55:33 +0100

    add logo to shared receipt

--- app/components/ReceiptImage.tsx
+++ app/components/ReceiptImage.tsx
@@ -6,7 +6,7 @@ import {useIPGeolocation} from '@/services/ip-geolocation';
 import {applyStyles, colors} from '@/styles';
 import {format} from 'date-fns';
 import React, {useCallback} from 'react';
-import {FlatList, ScrollView, Text, View, ViewStyle} from 'react-native';
+import {FlatList, ScrollView, Text, View, ViewStyle, Image} from 'react-native';
 import ViewShot, {ViewShotProperties} from 'react-native-view-shot';
 import RNFetchBlob from 'rn-fetch-blob';
 import {User} from 'types/app';
@@ -98,6 +98,17 @@ export const ReceiptImage = (props: Props) => {
           })}>
           <>
             <View style={applyStyles('py-lg items-center justify-center')}>
+              {!!businessInfo?.profile_image?.url && (
+                <Image
+                  style={applyStyles('mb-4 items-center justify-center', {
+                    width: 80,
+                    height: 80,
+                    borderRadius: 16,
+                    backgroundColor: colors['gray-20'],
+                  })}
+                  source={{uri: businessInfo?.profile_image.url}}
+                />
+              )}
               {!!businessInfo.name && (
                 <Text
                   style={applyStyles(
