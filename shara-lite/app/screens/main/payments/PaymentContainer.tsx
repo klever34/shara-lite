5dc34353dd5b2c2d5d2d5e4affa5be00f7e9872c
racheal58 <orukele_racheal@yahoo.com>
2020-11-30 15:21:50 +0100

    preview payment screen

--- shara-lite/app/screens/main/payments/PaymentContainer.tsx
+++ shara-lite/app/screens/main/payments/PaymentContainer.tsx
@@ -2,10 +2,13 @@ import {AppInput, Button, SecureEmblem} from '@/components';
 import {Icon} from '@/components/Icon';
 import Touchable from '@/components/Touchable';
 import {ModalWrapperFields, withModal} from '@/helpers/hocs';
+import {getAuthService} from '@/services';
+import {useIPGeolocation} from '@/services/ip-geolocation';
+import {useAppNavigation} from '@/services/navigation';
 import {applyStyles, colors} from '@/styles';
 import {Picker} from '@react-native-community/picker';
 import {Formik} from 'formik';
-import React, {useCallback, useState} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import {FlatList, Image, ScrollView, Text, View} from 'react-native';
 
 type PaymentMethod = {
@@ -21,7 +24,18 @@ const pickerLabelString = {
 
 function PaymentContainer(props: ModalWrapperFields) {
   const {openModal} = props;
+  const navigation = useAppNavigation();
+  const {callingCode} = useIPGeolocation();
   const [paymentMethods, setPaymentMethods] = useState<PaymentMethod[]>([]);
+  const [business, setBusiness] = useState(getAuthService().getBusinessInfo());
+
+  const getMobileNumber = useCallback(() => {
+    const code = business.country_code || callingCode;
+    if (business.mobile?.startsWith(code)) {
+      return `+${code}${business.mobile.replace(code, '')}`;
+    }
+    return `+${code}${business.mobile}`;
+  }, [business.country_code, business.mobile, callingCode]);
 
   const onFormSubmit = useCallback(
     (values) => {
@@ -182,24 +196,136 @@ function PaymentContainer(props: ModalWrapperFields) {
     openModal('full', {
       renderContent: () => (
         <View style={applyStyles('flex-1 bg-gray-10 px-16')}>
-          <View style={applyStyles('flex-row justify-between')}>
+          <View style={applyStyles('flex-row justify-between items-center')}>
             <Image
               resizeMode="contain"
               style={applyStyles('w-80 h-80')}
               source={require('@/assets/images/shara_logo_red.png')}
             />
-            <SecureEmblem />
+            <SecureEmblem style={applyStyles({width: 48, height: 48})} />
+          </View>
+          <View style={applyStyles('bg-white rounded-16 p-16 mb-24')}>
+            <View style={applyStyles('flex-row items-center')}>
+              <View style={applyStyles('w-80 h-80')}>
+                {business.profile_image && (
+                  <Image
+                    style={applyStyles('w-full h-full rounded-lg')}
+                    source={{
+                      uri: business.profile_image.url,
+                    }}
+                  />
+                )}
+              </View>
+              <View style={applyStyles('flex-1 px-12')}>
+                <Text
+                  style={applyStyles(
+                    'text-700 uppercase leading-16 text-gray-300 mb-4',
+                  )}>
+                  {business.name}
+                </Text>
+                <Text
+                  style={applyStyles(
+                    'text-400 text-sm leading-16 text-gray-300 mb-4',
+                  )}>
+                  {business.address}
+                </Text>
+                {!!business.mobile && (
+                  <Text
+                    style={applyStyles(
+                      'text-400 text-sm leading-16  text-gray-300 mb-4',
+                      {
+                        color: colors['gray-300'],
+                      },
+                    )}>
+                    Tel: {getMobileNumber()}
+                  </Text>
+                )}
+              </View>
+            </View>
+
+            <View>
+              <Text
+                style={applyStyles('text-gray-300 text-700 text-center py-24')}>
+                You can pay me via
+              </Text>
+              {paymentMethods.map((item, index) => (
+                <View
+                  key={`${item.id}-${index}`}
+                  style={applyStyles(
+                    'flex-row items-center py-8 bg-white justify-between',
+                    {
+                      borderTopColor: colors['gray-10'],
+                      borderTopWidth: 1,
+                      borderBottomColor: colors['gray-10'],
+                      borderBottomWidth: 1,
+                    },
+                  )}>
+                  <View style={applyStyles('py-8')}>
+                    <Text
+                      style={applyStyles(
+                        'pb-4 text-gray-200 text-uppercase text-xs',
+                      )}>
+                      {pickerLabelString[item.method]}
+                    </Text>
+                    <Text style={applyStyles('text-gray-300 text-700 text-lg')}>
+                      {item.number}
+                    </Text>
+                  </View>
+                  <View>
+                    <View
+                      style={applyStyles(
+                        'px-16 py-8 bg-gray-20 rounded-4 p-8',
+                      )}>
+                      <Text
+                        style={applyStyles(
+                          'text-uppercase text-400 text-gray-300 text-xs',
+                        )}>
+                        Copy number
+                      </Text>
+                    </View>
+                  </View>
+                </View>
+              ))}
+            </View>
+          </View>
+
+          <View style={applyStyles('center')}>
+            <Text style={applyStyles('text-center text-700 text-gray-300')}>
+              CREATE RECEIPTS WITH SHARA FOR FREE. DOWNLOAD NOW.
+            </Text>
+          </View>
+          <View style={applyStyles('bottom-16 absolute center')}>
+            <Text style={applyStyles('text-center text-gray-100 text-sm')}>
+              Powered by Shara Inc © 2020
+            </Text>
+            <Text style={applyStyles('text-center text-gray-100 text-sm')}>
+              www.shara.co
+            </Text>
           </View>
         </View>
       ),
     });
-  }, [openModal]);
+  }, [
+    business.address,
+    business.mobile,
+    business.name,
+    business.profile_image,
+    getMobileNumber,
+    openModal,
+    paymentMethods,
+  ]);
+
+  useEffect(() => {
+    return navigation.addListener('focus', () => {
+      setBusiness(getAuthService().getBusinessInfo());
+    });
+  }, [navigation]);
 
   return (
     <ScrollView
       persistentScrollbar
       keyboardShouldPersistTaps="always"
-      style={applyStyles('py-16 bg-white flex-1')}>
+      style={applyStyles('py-24 bg-white flex-1')}>
       {paymentMethods.length === 0 ? (
         <View style={applyStyles('px-16')}>
           <View style={applyStyles('center pb-32')}>
