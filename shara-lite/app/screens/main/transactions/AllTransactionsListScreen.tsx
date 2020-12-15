237381cfaba8426d7b3176c775b86f703685ef8f
Tioluwani Kolawole <kolawole8tiolu@gmail.com>
2020-12-15 12:27:53 +0100

    fix app crash on transaction search

--- shara-lite/app/screens/main/transactions/AllTransactionsListScreen.tsx
+++ shara-lite/app/screens/main/transactions/AllTransactionsListScreen.tsx
@@ -15,7 +15,7 @@ export const useReceiptList = () => {
   const receipts = getTransactions();
 
   const [searchTerm, setSearchTerm] = useState('');
-  const [allReceipts, setAllReceipts] = useState(receipts || []);
+  const [allReceipts, setAllReceipts] = useState(receipts);
 
   const handleReceiptSearch = useCallback((text) => {
     setSearchTerm(text);
@@ -34,16 +34,15 @@ export const useReceiptList = () => {
   }, [allReceipts, searchTerm]);
 
   const owedReceipts = useMemo(() => {
-    let userReceipts = (allReceipts.filter(
-      (item) => !item.isPaid,
-    ) as unknown) as Realm.Results<IReceipt & Realm.Object>;
+    let userReceipts = (allReceipts as unknown) as Realm.Results<
+      IReceipt & Realm.Object
+    >;
     if (searchTerm) {
-      userReceipts = userReceipts.filtered(
-        `customer.name CONTAINS[c] "${searchTerm}"`,
-      );
+      userReceipts = userReceipts
+        .filtered(`customer.name CONTAINS[c] "${searchTerm}"`)
+        .sorted('created_at', true);
     }
-    return (userReceipts as unknown) as IReceipt[];
-    // return userReceipts.sorted('created_at', true);
+    return userReceipts.filter((item) => !item.isPaid);
   }, [allReceipts, searchTerm]);
 
   useEffect(() => {
