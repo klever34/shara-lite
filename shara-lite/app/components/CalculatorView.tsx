d5653c38aa3097bc53f1bcb3a7618bb6f86a9235
Wahab Bello <wahaaabello@gmail.com>
2020-12-18 16:10:34 +0100

    feature: fix design of calculator
    Signed-off-by: Wahab Bello <wahaaabello@gmail.com>

--- shara-lite/app/components/CalculatorView.tsx
+++ shara-lite/app/components/CalculatorView.tsx
@@ -4,10 +4,11 @@ import React, {
   useState,
   createContext,
   useContext,
+  useEffect,
 } from 'react';
 import {AppInput, AppInputProps} from '@/components/AppInput';
 import {applyStyles} from '@/styles';
-import {Text, View} from 'react-native';
+import {View} from 'react-native';
 import Icon from '@/components/Icon';
 import {TransactionEntryButton} from '@/components/TransactionEntryView';
 import {Keyboard} from 'react-native-ui-lib';
@@ -15,9 +16,27 @@ const KeyboardUtils = Keyboard.KeyboardUtils;
 const KeyboardAccessoryView = Keyboard.KeyboardAccessoryView;
 const KeyboardRegistry = Keyboard.KeyboardRegistry;
 
+enum CalculatorKeyboardActions {
+  TYPE = 'TYPE',
+  DELETE = 'DELETE',
+  CLEAR = 'CLEAR',
+  ANSWER = 'ANSWER',
+}
+
+type CalculatorKeyboardItemSelectedEvent = {
+  action: CalculatorKeyboardActions;
+  value?: any;
+};
+type CalculatorKeyboardItemSelectedEventCallback = (
+  event: CalculatorKeyboardItemSelectedEvent,
+) => void;
+
 type CalculatorContextProps = {
   showKbComponent?: () => void;
   resetKbComponent?: () => void;
+  addEventListener?: (
+    callback: CalculatorKeyboardItemSelectedEventCallback,
+  ) => () => void;
 };
 
 const CalculatorContext = createContext<CalculatorContextProps>({});
@@ -34,27 +53,49 @@ export function CalculatorView({children}: CalculatorViewProps) {
   const resetKbComponent = useCallback(() => {
     setKbComponent(undefined);
   }, []);
+  const [itemSelectedListeners, setItemSelectedListeners] = useState<
+    CalculatorKeyboardItemSelectedEventCallback[]
+  >([]);
+  const addEventListener = useCallback(
+    (callback: CalculatorKeyboardItemSelectedEventCallback) => {
+      setItemSelectedListeners((prevItemSelectedListeners) => [
+        ...prevItemSelectedListeners,
+        callback,
+      ]);
+      return () => {
+        setItemSelectedListeners((prevItemSelectedListeners) => {
+          const index = prevItemSelectedListeners.findIndex(
+            (cb) => cb === callback,
+          );
+          return [
+            ...prevItemSelectedListeners.slice(0, index),
+            ...prevItemSelectedListeners.slice(index + 1),
+          ];
+        });
+      };
+    },
+    [],
+  );
+  const handleItemSelected = useCallback(
+    (name: string, event: CalculatorKeyboardItemSelectedEvent) => {
+      itemSelectedListeners.forEach((itemSelectedListener) => {
+        itemSelectedListener(event);
+      });
+    },
+    [itemSelectedListeners],
+  );
   return (
-    <CalculatorContext.Provider value={{showKbComponent, resetKbComponent}}>
+    <CalculatorContext.Provider
+      value={{showKbComponent, resetKbComponent, addEventListener}}>
       {children}
       <KeyboardAccessoryView
         kbComponent={kbComponent}
-        onItemSelected={(name: string, args: any) => {
-          console.log(name, args);
-          KeyboardUtils.dismiss();
-        }}
+        onItemSelected={handleItemSelected}
         onRequestShowKeyboard={showKbComponent}
         onKeyboardResigned={resetKbComponent}
       />
     </CalculatorContext.Provider>
   );
-}
-
-enum CalculatorKeyboardActions {
-  TYPE = 'TYPE',
-  DELETE = 'DELETE',
-  CLEAR = 'CLEAR',
-  ANSWER = 'ANSWER',
 }
 
 export const CalculatorKeyboardName = 'CalculatorKeyboard';
@@ -69,11 +110,11 @@ const CalculatorKeyboard = () => {
     };
   }, []);
 
-  const handleClear = useCallback(() => {
-    KeyboardRegistry.onItemSelected(CalculatorKeyboardName, {
-      action: CalculatorKeyboardActions.CLEAR,
-    });
-  }, []);
+  // const handleClear = useCallback(() => {
+  //   KeyboardRegistry.onItemSelected(CalculatorKeyboardName, {
+  //     action: CalculatorKeyboardActions.CLEAR,
+  //   });
+  // }, []);
 
   const handleDelete = useCallback(() => {
     KeyboardRegistry.onItemSelected(CalculatorKeyboardName, {
@@ -88,66 +129,55 @@ const CalculatorKeyboard = () => {
   }, []);
 
   return (
-    <View style={applyStyles('flex-1')}>
-      <View style={applyStyles('flex-row flex-1')}>
-        <TransactionEntryButton label="." onPress={enterValue('.')} />
-        <TransactionEntryButton
-          style={applyStyles('bg-gray-50', {flex: 2})}
-          label="clear"
-          onPress={handleClear}
-        />
-        <TransactionEntryButton
-          onPress={handleDelete}
-          style={applyStyles('bg-gray-50')}>
+    <View style={applyStyles('flex-1 flex-row px-16')}>
+      <View style={applyStyles('flex-1')}>
+        <TransactionEntryButton onPress={handleDelete}>
           <Icon type="feathericons" name="delete" size={24} />
         </TransactionEntryButton>
-      </View>
-      <View style={applyStyles('flex-row flex-1')}>
         <TransactionEntryButton label="7" onPress={enterValue('7')} />
-        <TransactionEntryButton label="8" onPress={enterValue('8')} />
-        <TransactionEntryButton label="9" onPress={enterValue('9')} />
-        <TransactionEntryButton
-          label="%"
-          onPress={enterValue('%')}
-          style={applyStyles('bg-gray-50')}
-        />
-      </View>
-      <View style={applyStyles('flex-row flex-1')}>
         <TransactionEntryButton label="4" onPress={enterValue('4')} />
+        <TransactionEntryButton label="1" onPress={enterValue('1')} />
+        <TransactionEntryButton label="00" onPress={enterValue('00')} />
+      </View>
+      <View style={applyStyles('flex-1')}>
+        <TransactionEntryButton label="%" onPress={enterValue('%')} />
+        <TransactionEntryButton label="8" onPress={enterValue('8')} />
         <TransactionEntryButton label="5" onPress={enterValue('5')} />
-        <TransactionEntryButton label="6" onPress={enterValue('6')} />
+        <TransactionEntryButton label="2" onPress={enterValue('2')} />
+        <TransactionEntryButton label="0" onPress={enterValue('0')} />
+      </View>
+      <View style={applyStyles('flex-1')}>
         <TransactionEntryButton
           label="÷"
           onPress={enterValue('÷')}
-          style={applyStyles('bg-gray-50')}
+          textStyle={applyStyles('text-red-100')}
         />
-      </View>
-      <View style={applyStyles('flex-row flex-1')}>
-        <TransactionEntryButton label="1" onPress={enterValue('1')} />
-        <TransactionEntryButton label="2" onPress={enterValue('2')} />
+        <TransactionEntryButton label="9" onPress={enterValue('9')} />
+        <TransactionEntryButton label="6" onPress={enterValue('6')} />
         <TransactionEntryButton label="3" onPress={enterValue('3')} />
+        <TransactionEntryButton label="." onPress={enterValue('.')} />
+      </View>
+      <View style={applyStyles('flex-1')}>
         <TransactionEntryButton
+          label="x"
           onPress={enterValue('*')}
-          style={applyStyles('bg-gray-50')}>
-          <Text>x</Text>
-        </TransactionEntryButton>
-      </View>
-      <View style={applyStyles('flex-row flex-1')}>
-        <TransactionEntryButton label="0" onPress={enterValue('0')} />
-        <TransactionEntryButton label="00" onPress={enterValue('00')} />
-        <TransactionEntryButton label="000" onPress={enterValue('000')} />
-        <TransactionEntryButton label="=" onPress={handleCalculate} />
-      </View>
-      <View style={applyStyles('flex-row flex-1')}>
+          textStyle={applyStyles('text-red-100')}
+        />
         <TransactionEntryButton
           label="-"
           onPress={enterValue('-')}
-          style={applyStyles('bg-gray-50')}
+          textStyle={applyStyles('text-red-100')}
         />
         <TransactionEntryButton
           label="+"
           onPress={enterValue('+')}
-          style={applyStyles('bg-gray-50')}
+          textStyle={applyStyles('text-red-100')}
+        />
+        <TransactionEntryButton
+          label="="
+          onPress={handleCalculate}
+          style={applyStyles('bg-red-200', {flex: 2.1})}
+          textStyle={applyStyles('text-white')}
         />
       </View>
     </View>
@@ -162,16 +192,24 @@ KeyboardRegistry.registerKeyboard(
 type CalculatorInputProps = AppInputProps & {};
 
 export const CalculatorInput = (props: CalculatorInputProps) => {
-  const {showKbComponent, resetKbComponent} = useContext(CalculatorContext);
+  const {showKbComponent, resetKbComponent, addEventListener} = useContext(
+    CalculatorContext,
+  );
+  useEffect(() => {
+    if (addEventListener) {
+      addEventListener((event) => {
+        console.log('event', event);
+        KeyboardUtils.dismiss();
+      });
+    }
+  }, [addEventListener]);
   return (
-    <>
-      <AppInput
-        {...props}
-        onFocus={showKbComponent}
-        onTouchStart={showKbComponent}
-        onBlur={resetKbComponent}
-        showSoftInputOnFocus={false}
-      />
-    </>
+    <AppInput
+      {...props}
+      onFocus={showKbComponent}
+      onTouchStart={showKbComponent}
+      onBlur={resetKbComponent}
+      showSoftInputOnFocus={false}
+    />
   );
 };
