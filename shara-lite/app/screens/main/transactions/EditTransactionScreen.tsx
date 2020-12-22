79459087bef956efc677fef368d670689a96af30
Wahab Bello <wahaaabello@gmail.com>
2020-12-22 16:39:30 +0100

    fix: show keyboard on edit transaction
    Signed-off-by: Wahab Bello <wahaaabello@gmail.com>

--- shara-lite/app/screens/main/transactions/EditTransactionScreen.tsx
+++ shara-lite/app/screens/main/transactions/EditTransactionScreen.tsx
@@ -9,6 +9,7 @@ import React, {useCallback, useContext} from 'react';
 import {Alert, SafeAreaView, Text, View} from 'react-native';
 import {ScrollView} from 'react-native-gesture-handler';
 import {MainStackParamList} from '..';
+import {CalculatorView} from '@/components/CalculatorView';
 
 type EditTransactionScreenProps = {
   route: RouteProp<MainStackParamList, 'EditTransaction'>;
@@ -38,29 +39,31 @@ export const EditTransactionScreen = (props: EditTransactionScreenProps) => {
   );
 
   return (
-    <SafeAreaView style={applyStyles('flex-1 bg-white')}>
-      <ScrollView
-        style={applyStyles('flex-1')}
-        keyboardShouldPersistTaps="always">
-        <View
-          style={applyStyles(
-            'flex-row py-8 pr-16 bg-white items-center justify-between',
-            {
-              borderBottomWidth: 1.5,
-              borderBottomColor: colors['gray-20'],
-            },
-          )}>
-          <HeaderBackButton iconName="arrow-left" />
-        </View>
-        <View style={applyStyles('px-16')}>
-          <View style={applyStyles('pt-16 pb-32')}>
-            <Text style={applyStyles('text-gray-300 text-400 text-xl')}>
-              Edit Transaction
-            </Text>
+    <CalculatorView>
+      <SafeAreaView style={applyStyles('flex-1 bg-white')}>
+        <ScrollView
+          style={applyStyles('flex-1')}
+          keyboardShouldPersistTaps="always">
+          <View
+            style={applyStyles(
+              'flex-row py-8 pr-16 bg-white items-center justify-between',
+              {
+                borderBottomWidth: 1.5,
+                borderBottomColor: colors['gray-20'],
+              },
+            )}>
+            <HeaderBackButton iconName="arrow-left" />
           </View>
-          <RecordSaleForm transaction={transaction} onSubmit={handleSave} />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+          <View style={applyStyles('px-16')}>
+            <View style={applyStyles('pt-16 pb-32')}>
+              <Text style={applyStyles('text-gray-300 text-400 text-xl')}>
+                Edit Transaction
+              </Text>
+            </View>
+            <RecordSaleForm transaction={transaction} onSubmit={handleSave} />
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </CalculatorView>
   );
 };
