8f7201cbdc00670bb4223ede578b460cd8381a88
Wahab Bello <wahaaabello@gmail.com>
2021-02-11 14:24:21 +0100

    chore: update script
    Signed-off-by: Wahab Bello <wahaaabello@gmail.com>

--- shara-lite/app/services/i18n/translations/script.ts
+++ shara-lite/app/services/i18n/translations/script.ts
@@ -2,57 +2,74 @@ import csvParser from 'csv-parser';
 import fs from 'fs';
 import _ from 'lodash';
 import {writeSync as copyToClipboard} from 'clipboardy';
+import offlineTranslations from '.';
 
-// console.log(
-//   (() => {
-//     return JSON.stringify(translations);
-//   })(),
-// );
-//
-// console.log(
-//   (() => {
-//     const generate = (object: {[key: string]: any}, prefix = '') => {
-//       let file = '';
-//       Object.keys(object).forEach((name) => {
-//         const key = `${prefix}${prefix ? '.' : ''}${name}`;
-//         const value = object[name];
-//         if (typeof value === 'string') {
-//           file += `${key},`;
-//           file += `"${value}"\n`;
-//         } else if (typeof value === 'object') {
-//           file += generate(value, key);
-//         }
-//       });
-//       return file;
-//     };
-//     let file = 'Key,';
-//     Object.keys(translations).forEach((langCode) => {
-//       file += langCode;
-//     });
-//     file += '\n';
-//     return file + generate(translations.en);
-//   })(),
-// );
-
-const compiled: {[key: string]: any} = {};
-
-fs.createReadStream('translations.csv')
-  .pipe(csvParser())
-  .on('data', ({key, ...translations}) => {
-    Object.keys(translations).forEach((language) => {
-      const [languageCode] = language.split(' ');
-      if (!(languageCode in compiled)) {
-        compiled[languageCode] = {};
+const generateCSV = (object: {[key: string]: any}, prefix = '') => {
+  let file = '';
+  Object.keys(object).forEach((name) => {
+    const key = `${prefix}${prefix ? '.' : ''}${name}`;
+    let value = object[name];
+    if (typeof value === 'string') {
+      if (value.includes(',')) {
+        value = `"${value}"`;
       }
-      _.set(compiled[languageCode], key, translations[language]);
-    });
-  })
-  .on('end', () => {
-    copyToClipboard(JSON.stringify(compiled));
-    fs.writeFile('translations.json', JSON.stringify(compiled), (err) => {
+      file += `${key},`;
+      file += `${value}\n`;
+    } else if (typeof value === 'object') {
+      file += generateCSV(value, key);
+    }
+  });
+  return file;
+};
+
+const readCSV = (filename: string): Promise<{[key: string]: string}[]> => {
+  const result: {[key: string]: string}[] = [];
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(filename)
+      .pipe(csvParser())
+      .on('data', (data) => {
+        result.push(data);
+      })
+      .on('error', reject)
+      .on('end', () => {
+        resolve(result);
+      });
+  });
+};
+
+const writeToFile = (filename: string, content: string): Promise<void> => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(filename, content, (err) => {
       if (err) {
-        throw err;
+        reject(err);
       }
-      console.log('done');
+      resolve();
     });
   });
+};
+
+let offlineTranslationsFile = 'key,';
+Object.keys(offlineTranslations).forEach((langCode) => {
+  offlineTranslationsFile += langCode;
+});
+offlineTranslationsFile += '\n';
+offlineTranslationsFile += generateCSV(offlineTranslations.en);
+
+writeToFile('offline-translations.json', JSON.stringify(offlineTranslations));
+writeToFile('offline-translations.csv', offlineTranslationsFile);
+
+const transactions: {[key: string]: any} = {};
+
+readCSV('translations.csv').then((data) => {
+  data.forEach(({key, ...translations}) => {
+    Object.keys(translations).forEach((language) => {
+      const [languageCode] = language.split(' ');
+      if (!(languageCode in transactions)) {
+        transactions[languageCode] = {};
+      }
+      _.set(transactions[languageCode], key, translations[language]);
+    });
+  });
+  copyToClipboard(JSON.stringify(transactions));
+  writeToFile('translations.json', JSON.stringify(transactions));
+});
