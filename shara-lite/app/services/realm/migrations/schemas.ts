818b02c4e036a31239aba83812cb185d19b30bb3
Olawale Malomo <warlay234@gmail.com>
2021-02-17 11:11:18 +0100

    Add wallet migration

--- shara-lite/app/services/realm/migrations/schemas.ts
+++ shara-lite/app/services/realm/migrations/schemas.ts
@@ -17,6 +17,7 @@ import schema16 from './1611236083669-add-activity-x-disable-customer-reminders'
 import schema17 from './1611256639370-re-add-disable-customer-reminders';
 import schema18 from './1613468659343-add-shara-pay-models';
 import schema19 from './1613546853297-add-is_primary_to_collection_disbursment_methods';
+import schema20 from './1613556320339-merchant-id-to-wallet';
 
 const schemas: any[] = [
   {schemaVersion: 1, ...schema1},
@@ -39,6 +40,7 @@ const schemas: any[] = [
   {schemaVersion: 18, ...schema18},
   {schemaVersion: 18, ...schema18},
   {schemaVersion: 19, ...schema19},
+  {schemaVersion: 20, ...schema20},
 ];
 
 export default schemas;
