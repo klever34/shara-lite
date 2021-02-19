52094fce351b113a2ddac949e99187491a7ccdad
Wahab Bello <wahaaabello@gmail.com>
2021-02-19 14:16:16 +0100

    feature: show deposit account
    Signed-off-by: Wahab Bello <wahaaabello@gmail.com>

--- shara-lite/app/screens/main/money/MoneyDepositScreen.tsx
+++ shara-lite/app/screens/main/money/MoneyDepositScreen.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import {Header, Text, Button} from '@/components';
+import React, {useCallback} from 'react';
+import {Button, Header, Text} from '@/components';
 import {getI18nService} from '@/services';
 import {View} from 'react-native';
 import {as} from '@/styles';
 import {useClipboard} from '@/helpers/hooks';
 import Touchable from '@/components/Touchable';
+import {useCollectionMethod} from '@/services/collection-method';
+import {useWallet} from '@/services/wallet';
 
 const strings = getI18nService().strings;
 
@@ -31,7 +33,7 @@ const AccountDetailsCard = ({
         <Text style={as(' text-gray-100')}>{label}</Text>
         <Text style={as('text-blue-100 font-bold text-2xl')}>{accountNo}</Text>
         {!!bankName && (
-          <Text style={as('capitalize text-black font-bold')}>{bankName}</Text>
+          <Text style={as('uppercase text-black font-bold')}>{bankName}</Text>
         )}
         <Text style={as('text-sm text-gray-50')}>{copyMessage}</Text>
       </View>
@@ -44,9 +46,21 @@ type MoneyDepositScreenProps = {
 };
 
 export const MoneyDepositScreen = ({onClose}: MoneyDepositScreenProps) => {
-  const merchantId = '12345667780';
-  const virtualBankAccountNo = '12345667780';
-  const virtualBankName = 'WEMA BANK';
+  const {getWallet} = useWallet();
+  const {getCollectionMethods} = useCollectionMethod();
+  const wallet = getWallet();
+  const collectionMethods = getCollectionMethods();
+  const merchantId = wallet?.merchant_id;
+  const parseBankDetails = useCallback((bankDetailsString: string): {
+    vnuban: string;
+    bank_name?: string;
+  } | null => {
+    try {
+      return JSON.parse(bankDetailsString);
+    } catch (e) {
+      return null;
+    }
+  }, []);
   return (
     <View style={as('')}>
       <Header
@@ -57,19 +71,32 @@ export const MoneyDepositScreen = ({onClose}: MoneyDepositScreenProps) => {
         <Text style={as('text-center text-sm mb-12 text-gray-200')}>
           {strings('payment_activities.deposit_help_text')}
         </Text>
-        <AccountDetailsCard
-          label={strings('payment_activities.your_merchant_id_is')}
-          accountNo={merchantId}
-          copyMessage={strings('payment_activities.tap_to_copy_merchant_id')}
-        />
-        <AccountDetailsCard
-          label={strings('payment_activities.your_wallet_account_no_is')}
-          accountNo={virtualBankAccountNo}
-          bankName={virtualBankName}
-          copyMessage={strings(
-            'payment_activities.tap_to_copy_wallet_account_no',
-          )}
-        />
+        {merchantId && (
+          <AccountDetailsCard
+            label={strings('payment_activities.your_merchant_id_is')}
+            accountNo={merchantId}
+            copyMessage={strings('payment_activities.tap_to_copy_merchant_id')}
+          />
+        )}
+        {collectionMethods.map((collectionMethod) => {
+          const bankDetails = parseBankDetails(
+            collectionMethod.account_details,
+          );
+          if (!bankDetails) {
+            return null;
+          }
+          return (
+            <AccountDetailsCard
+              key={bankDetails.vnuban}
+              label={strings('payment_activities.your_wallet_account_no_is')}
+              accountNo={bankDetails.vnuban}
+              bankName={bankDetails.bank_name}
+              copyMessage={strings(
+                'payment_activities.tap_to_copy_wallet_account_no',
+              )}
+            />
+          );
+        })}
         <Button
           title={strings('close')}
           variantColor="clear"
