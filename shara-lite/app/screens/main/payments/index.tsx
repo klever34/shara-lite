dbcb1ccedffb86f42f9364d361f4798b8d61069f
racheal58 <orukele_racheal@yahoo.com>
2021-03-03 19:58:49 +0100

    change screen when shara money is available in that counrty or not

--- shara-lite/app/screens/main/payments/index.tsx
+++ shara-lite/app/screens/main/payments/index.tsx
@@ -1,22 +1,42 @@
-import React from 'react';
+import {getAuthService, getRemoteConfigService} from '@/services';
+import React, {useMemo} from 'react';
 import {createNativeStackNavigator} from 'react-native-screens/native-stack';
+import PaymentContainer from './PaymentContainer';
 import {PaymentListScreen} from './PaymentListScreen';
 
 export type ProductsStackParamList = {
   ProductList: undefined;
   ManageItems: undefined;
+  WithdrawalMethod: undefined;
 };
 
 const ProductsStack = createNativeStackNavigator<ProductsStackParamList>();
 
 export const PaymentsScreen = () => {
+  const initialRouteName = useMemo(() => {
+    try {
+      const sharaMoneyEnabledCountries = getRemoteConfigService()
+        .getValue('sharaMoneyEnabledCountries')
+        .asString();
+      const user = getAuthService().getUser();
+      return JSON.parse(sharaMoneyEnabledCountries)[user?.currency_code ?? '']
+        ? 'WithdrawalMethod'
+        : 'ProductList';
+    } catch (e) {
+      return 'ProductList';
+    }
+  }, []);
   return (
     <ProductsStack.Navigator
-      initialRouteName="ProductList"
+      initialRouteName={initialRouteName}
       screenOptions={{
         headerShown: false,
       }}>
-      <ProductsStack.Screen name="ProductList" component={PaymentListScreen} />
+      <ProductsStack.Screen
+        name="WithdrawalMethod"
+        component={PaymentListScreen}
+      />
+      <ProductsStack.Screen name="ProductList" component={PaymentContainer} />
     </ProductsStack.Navigator>
   );
 };
