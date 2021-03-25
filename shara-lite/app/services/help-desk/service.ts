9058733103737171ab45dc11a64773910c45791b
Wahab Bello <wahaaabello@gmail.com>
2021-03-25 11:42:01 +0100

    Revert "chore: test keys"
    This reverts commit a6d5e59dd3587a5ee4669e81f3cc4f35b8369f1f.

--- shara-lite/app/services/help-desk/service.ts
+++ shara-lite/app/services/help-desk/service.ts
@@ -1,7 +1,6 @@
 import RNZendeskChat from 'react-native-zendesk-v2';
 import {User} from 'types/app';
 import Config from 'react-native-config';
-import {Alert} from 'react-native';
 
 export interface IHelpDeskService {
   setUser(user: User): void;
@@ -11,16 +10,6 @@ export interface IHelpDeskService {
 export class HelpDeskService implements IHelpDeskService {
   private user: User | null = null;
   constructor() {
-    if (!Config.ZENDESK_CHAT_ACCOUNT_KEY) {
-      Alert.alert('CHAT_ACCOUNT_KEY not set');
-      return;
-    }
-    Alert.alert(
-      Config.ZENDESK_CHAT_ACCOUNT_KEY +
-        Config.ZENDESK_APP_ID +
-        Config.ZENDESK_URL +
-        Config.ZENDESK_CLIENT_ID,
-    );
     RNZendeskChat.init({
       key: Config.ZENDESK_CHAT_ACCOUNT_KEY,
       appId: Config.ZENDESK_APP_ID,
@@ -29,9 +18,6 @@ export class HelpDeskService implements IHelpDeskService {
     });
   }
   setUser(user: User) {
-    if (!Config.ZENDESK_CHAT_ACCOUNT_KEY) {
-      return;
-    }
     this.user = user;
     // @ts-ignore
     RNZendeskChat.setUserIdentity({
